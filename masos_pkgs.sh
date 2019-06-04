66178579e7effb297bc514e0b4b28e1f5f5becbf
Moriggy <alvar.gm@gmail.com>
2019-06-04 16:27:26 +0100

    primera version

---
+++ masos_pkgs.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+# This file is part of The MasOS Project
+#
+# The MasOS Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/MasOS/MasOS-Setup/master/LICENSE.md
+#
+
+__version="0.1.1"
+
+[[ "$__debug" -eq 1 ]] && set -x
+
+# main masos install location
+rootdir="/opt/masos"
+
+# if __user is set, try and install for that user, else use SUDO_USER
+if [[ -n "$__user" ]]; then
+    user="$__user"
+    if ! id -u "$__user" &>/dev/null; then
+        echo "User $__user not exist"
+        exit 1
+    fi
+else
+    user="$SUDO_USER"
+    [[ -z "$user" ]] && user="$(id -un)"
+fi
+
+home="$(eval echo ~$user)"
+datadir="$home/MasOS"
+biosdir="$datadir/BIOS"
+romdir="$datadir/roms"
+emudir="$rootdir/emulators"
+configdir="$rootdir/configs"
+
+scriptdir="$(dirname "$0")"
+scriptdir="$(cd "$scriptdir" && pwd)"
+
+__logdir="$scriptdir/logs"
+__tmpdir="$scriptdir/tmp"
+__builddir="$__tmpdir/build"
+__swapdir="$__tmpdir"
+
+# check, if sudo is used
+if [[ "$(id -u)" -ne 0 ]]; then
+    echo "Script must be run under sudo from the user you want to install for. Try 'sudo $0'"
+    exit 1
+fi
+
+__backtitle="masos.dx.am - MasOS Setup. Directorio de instalacion: $rootdir para el usuario: $user"
+
+source "$scriptdir/scriptmodules/system.sh"
+source "$scriptdir/scriptmodules/helpers.sh"
+source "$scriptdir/scriptmodules/inifuncs.sh"
+source "$scriptdir/scriptmodules/packages.sh"
+
+setup_env
+
+rp_registerAllModules
+
+ensureFBMode 320 240
+
+rp_ret=0
+if [[ $# -gt 0 ]]; then
+    setupDirectories
+    rp_callModule "$@"
+    rp_ret=$?
+else
+    rp_printUsageinfo
+fi
+
+printMsgs "console" "${__INFMSGS[@]}"
+exit $rp_ret
